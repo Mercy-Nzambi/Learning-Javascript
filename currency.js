c7ce9fb99d167ec87cba76363e473bbb85862701
Mercy-Nzambi <146635465+Mercy-Nzambi@users.noreply.github.com>
2024-08-05 00:58:32 +0300

    currency.js

---
+++ currency.js
@@ -0,0 +1,15 @@
+// Write code below 💖
+
+//converting leftover cash from the trip to USD 
+
+let yuan = 560;
+let yen = 2455;
+let won = 3280;
+
+total = (yuan*0.14)+(yen*0.0068)+(won*0.00074)
+
+console.log('What do you have left in yuan? 560');
+console.log('What do you have left in yen? 2455');
+console.log('What do you have left in won? 3280');
+
+console.log(total);
